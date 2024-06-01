7863b6943192169775b07a69b9c8345f829bbdf5
Shailaj Bashyal <shailaj@pop-os.localdomain>
2024-06-01 10:30:24 +0545

    first comit

---
+++ main.c
@@ -0,0 +1,148 @@
+
+#include <stdio.h>
+
+//functions for each data Structures
+void array();
+void queue();
+void stack();
+
+
+//functions for array
+void insert();
+void delete();
+void display_index();
+
+
+//functions for queue
+void enqueue();
+void dequeue();
+
+//functions for stack
+void pop();
+void push();
+
+//general functions
+void display();
+
+
+
+//structures for data structures
+typedef struct{
+    int ar[5];
+    int index;
+}Array;
+
+
+typedef struct{
+    int data[5];
+    int front;
+    int rear;
+}Queue;
+
+
+typedef struct{
+    int data[5];
+    int top;
+    int bottom;
+}Stack;
+
+
+int main()
+{
+    int ch;
+    int ch1=1;
+    while(ch1)
+    {
+        printf("Implementation of Data Structures \n\n\n");
+        printf("What do you want to create?\n1. Array\n2. Queue\n3. Stack\n");
+        scanf("%d",&ch);
+        
+        ch1 = 0;
+        switch(ch)
+        {
+            case 1:
+                array();
+                break;
+            
+            case 2:
+                queue();
+                break;
+                
+            case 3:
+                stack();
+                break;
+                
+            default:
+                printf("Wrong input\n\n");
+                ch1=1;
+        }
+    }    
+    
+    return 0;
+}
+
+//In Array functions
+
+void insert()
+{
+    printf("\n\nIn which index do you want to insert in? (0-4)\n");
+    scanf("%d",&);
+    return;
+}
+    
+void delete()
+{
+    return;
+}
+
+void display()
+{
+    return;
+}
+
+void display_index()
+{
+    return;
+}
+
+
+
+
+void array()
+{
+    int choice;
+    printf("\n\n\nWhat do you want to do?\n1.Insert\n2.Delete\n3.Display\n4.Display a value from index\n5.End\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+            insert();
+            break;
+            
+        case 2:
+            delete();
+            break;
+            
+        case 3:
+            display();
+            break;
+            
+        case 4:
+            display_index();
+            break;
+            
+        default:
+            printf("Wrong input\n\n");
+
+    }
+}
+
+void queue()
+{
+    return;
+}
+
+void stack()
+{
+    return;
+}
---
+++ try.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2] = {0};
+    printf("%d    %d",a[0],a[1]);
+    return 0;
+}
